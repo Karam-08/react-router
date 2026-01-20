5a327685458b0f929d8461edf01fb332c6b13b78
Karam Abbas <kma69362@gmail.com>
2026-01-20 15:19:27 -0700

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
 import {Link} from 'react-router-dom'
 
 const Home = () => {
-  return (
-    <div className='card'>
-        <h2>Home</h2>
-        <p className="small">This is a mini project that demonstrates React Router DOM, routes, params, nested routes, and prtected routes along with page navigation.</p>
-        <div className="row">
-            <Link to="/courses" className="link active">Browse Courses</Link>
-            <Link to="/dashboard" className="link active">Go To Dashboard</Link>
-        </div>
+    return (
+        <div className='card'>
+            <h2>Home</h2>
+            <p className="small">This is a mini project that demonstrates React Router DOM, routes, params, nested routes, and prtected routes along with page navigation.</p>
+            <div className="row">
+                <Link to="/courses" className="link active">Browse Courses</Link>
+                <Link to="/dashboard" className="link active">Go To Dashboard</Link>
+            </div>
 
-        <hr />
-        <p className="small">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, repellat vitae. Fuga adipisci quas laboriosam at temporibus animi pariatur in exercitationem quibusdam, soluta aut sunt.
-        </p>
-    </div>
-  )
+            <hr/>
+            <p className="small">   
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, repellat vitae. Fuga adipisci quas laboriosam at temporibus animi pariatur in exercitationem quibusdam, soluta aut sunt.
+            </p>
+        </div>
+    )
 }
 
 export default Home
